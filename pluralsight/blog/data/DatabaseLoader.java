70364f8750fedeee6e6c45be64c5ab6808181013
Charan R <96077678+Charan-R-bit@users.noreply.github.com>
2023-02-06 17:40:25 +0530

    Add files via upload

--- pluralsight/blog/data/DatabaseLoader.java
+++ pluralsight/blog/data/DatabaseLoader.java
@@ -1,36 +1,44 @@
 package com.pluralsight.blog.data;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
-
+import com.pluralsight.blog.model.Author;
+import com.pluralsight.blog.model.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-import com.pluralsight.blog.model.Author;
-import com.pluralsight.blog.model.Post;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
 
 @Component
 public class DatabaseLoader implements ApplicationRunner {
-	@Autowired
-	private final PostRepository postRepository;
-	private final String[] templates = {
+    private final String[] templates = {
             "Smart Home %s", "Mobile %s - For When You're On he Go", "The %s - Your New Favorite Accessory"};
     private final String[] gadgets = {
             "Earbuds", "Speakers", "Tripod", "Instant Pot", "Coffee Cup", "Keyboard", "Sunglasses"};
     public List<Post> randomPosts = new ArrayList<>();
     public List<Author> authors = new ArrayList<>();
 
-    
-    public DatabaseLoader(PostRepository postRepository) {
-    	this.postRepository = postRepository;
-    	
+    private final PostRepository postRepository;
+    private final AuthorRepository authorRepository;
+
+    @Autowired
+    public DatabaseLoader(PostRepository postRepository, AuthorRepository authorRepository) {
+        this.postRepository = postRepository;
+        this.authorRepository = authorRepository;
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        authors.addAll(Arrays.asList(
+                new Author("sholderness", "Sarah",  "Holderness", "password"),
+                new Author("tbell", "Tom",  "Bell", "password"),
+                new Author("efisher", "Eric",  "Fisher", "password"),
+                new Author("csouza", "Carlos",  "Souza", "password")
+        ));
+        authorRepository.saveAll(authors);
         IntStream.range(0,40).forEach(i->{
             String template = templates[i % templates.length];
             String gadget = gadgets[i % gadgets.length];
@@ -38,7 +46,11 @@ public class DatabaseLoader implements ApplicationRunner {
             String title = String.format(template, gadget);
             Post post = new Post(title, "Lorem ipsum dolor sit amet, consectetur adipiscing elit… ");
             randomPosts.add(post);
+            Author author = authors.get(authors.size()-1);
+            post.setAuthor(author);
+            author.addPost(post);
         });
         postRepository.saveAll(randomPosts);
+        authorRepository.saveAll(authors);
     }
 }
--- pluralsight/blog/data/PostRepository.java
+++ pluralsight/blog/data/PostRepository.java
@@ -1,13 +1,16 @@
 package com.pluralsight.blog.data;
 
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.pluralsight.blog.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-public interface PostRepository extends JpaRepository<Post, Long> 
-{
-	public List<Post> findByTitleContaining(String title);
-	}
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long>{
+
+    @RestResource(rel = "contains-title", path = "containsTitle")
+    List<Post> findByTitleContaining(String title);
+
+    List<Post> findByAuthor_Lastname (String lastname);
+
+}
